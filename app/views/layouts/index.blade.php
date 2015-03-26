57e126fe0ec3b3f57445adbcb3565ee9a0c24829
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-26 19:53:11 -0300

    Adicionado template do AdminLte

--- app/views/layouts/index.blade.php
+++ app/views/layouts/index.blade.php
@@ -1,27 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
- 	<head>
-    	<meta charset="utf-8">
-    	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8">
+    <title>Sigimob</title>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <!-- Bootstrap 3.3.2 -->
+    {{ HTML::style('bootstrap/css/bootstrap.min.css') }}
+    <!-- Font Awesome Icons -->
+    {{ HTML::style('font-awesome-4.3.0/css/font-awesome.min.css') }}
+    <!-- Theme style -->
+    {{ HTML::style('css/AdminLTE.min.css') }}
+    <!-- iCheck -->
+    {{ HTML::style('plugins/iCheck/square/blue.css') }}
 
-    	<title>Sigimob</title>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body class="login-page">
+    <div class="login-box">
+      <div class="login-logo">
+        <a href="../../index2.html"><b>Admin</b>LTE</a>
+      </div><!-- /.login-logo -->
+      <div class="login-box-body">
+        <p class="login-box-msg">Sign in to start your session</p>
+        <form action="../../index2.html" method="post">
+          <div class="form-group has-feedback">
+            <input type="text" class="form-control" placeholder="Email"/>
+            <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+          </div>
+          <div class="form-group has-feedback">
+            <input type="password" class="form-control" placeholder="Password"/>
+            <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+          </div>
+          <div class="row">
+            <div class="col-xs-8">    
+              <div class="checkbox icheck">
+                <label>
+                  <input type="checkbox"> Remember Me
+                </label>
+              </div>                        
+            </div><!-- /.col -->
+            <div class="col-xs-4">
+              <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+            </div><!-- /.col -->
+          </div>
+        </form>
 
-    	{{ HTML::style('packages/bootstrap/css/bootstrap.min.css') }}
-    	{{ HTML::style('css/main.css')}}
-  	</head>
+        <div class="social-auth-links text-center">
+          <p>- OR -</p>
+          <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using Facebook</a>
+          <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
+        </div><!-- /.social-auth-links -->
 
-  	<body>
+        <a href="#">I forgot my password</a><br>
+        <a href="register.html" class="text-center">Register a new membership</a>
 
-	  	<div class="navbar navbar-fixed-top">
-		  	<div class="navbar-inner">
-		    	<div class="container">
-					oiiiiiii
-		    	</div>
-		  	</div>
-		</div> 	            
+      </div><!-- /.login-box-body -->
+    </div><!-- /.login-box -->
 
-	    <div class="container">
-	    </div>
-
-  	</body>
+    <!-- jQuery 2.1.3 -->
+    {{ HTML::script('plugins/jQuery/jQuery-2.1.3.min.js') }}
+    <!-- Bootstrap 3.3.2 JS -->
+    {{ HTML::script('bootstrap/js/bootstrap.min.js') }}
+    <!-- iCheck -->
+    {{ HTML::script('plugins/iCheck/icheck.min.js') }}
+    <script>
+      $(function () {
+        $('input').iCheck({
+          checkboxClass: 'icheckbox_square-blue',
+          radioClass: 'iradio_square-blue',
+          increaseArea: '20%' // optional
+        });
+      });
+    </script>
+  </body>
 </html>
