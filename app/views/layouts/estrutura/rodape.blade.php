f151e274d1a160d067a952c4ef8649f512f4000c
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-27 20:27:30 -0300

    Criado pasta de parciais da estrutura

---
+++ app/views/layouts/estrutura/rodape.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Versão</b> 1.0
+    </div>
+    <strong>Copyright &copy; 2015 <a href="http://sistemasguarani.esy.es">Sistemas Guarani</a>.</strong> Todos os direitos reservados.
+</footer>
