054490fdb75ee5e4886f6ec6a49c3e1e1dea7611
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-27 20:27:58 -0300

    Criado parcial de rodape

--- app/views/layouts/index.blade.php
+++ app/views/layouts/index.blade.php
@@ -144,12 +144,7 @@
         </section><!-- /.content -->
       </div><!-- /.content-wrapper -->
 
-      <footer class="main-footer">
-        <div class="pull-right hidden-xs">
-          <b>Versão</b> 1.0
-        </div>
-        <strong>Copyright &copy; 2015 <a href="http://sistemasguarani.esy.es">Sistemas Guarani</a>.</strong> Todos os direitos reservados.
-      </footer>
+      @include('layouts.estrutura.rodape')
 
     </div><!-- ./wrapper -->
 
