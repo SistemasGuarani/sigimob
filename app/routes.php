4cd7e4184fb617322b551dfe4d620a76b69d4055
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-27 19:56:47 -0300

    Criado rotas iniciais do sistema

--- app/routes.php
+++ app/routes.php
@@ -3,3 +3,17 @@
 Route::get('/', 'LoginController@login');
 
 Route::get('/index', 'IndexController@index');
+
+Route::get('/cadastrar', function(){
+	$user = new user;
+
+	$user->email = "romulo@proesc.com";
+	$user->real_name = "Romulo Mendes";
+	$user->password = "coringa";
+
+	$user->save();
+	
+	return "Salvo com sucesso";
+});
+
+Route::get('/users', 'UserController@index');
