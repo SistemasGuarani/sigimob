e00ee0b38707a468978eec5c4c0bb5329a30f2ed
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-28 17:32:23 -0300

    Montado estrutura

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -1,11 +1,16 @@
-<title>sdadsa</title>
-<h1>Lista de Usuarios</h1>
-@if ($users)
-<ul>
-	@foreach($users as $user)
-	<li>{{ $user->real_name }} - {{ $user->email }}</li>
-	@endforeach
-</ul>
-@else
-	Nenhum Usuario cadastrado no sistema
-@endif
+@extends('layouts.index')
+
+@section('titulo')
+	Usuários
+@stop
+@section('conteudo')
+	@if ($users)
+	<ul>
+		@foreach($users as $user)
+		<li>{{ $user->real_name }} - {{ $user->email }}</li>
+		@endforeach
+	</ul>
+	@else
+		Nenhum Usuario cadastrado no sistema
+	@endif
+@stop
