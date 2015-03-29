e41e71d68d7098f6a1455d9c4757f82716db2043
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-29 20:33:49 -0300

    Alterado CSS do rodapé e retirado imagem de fundo

--- app/views/site/localizar.blade.php
+++ app/views/site/localizar.blade.php
@@ -16,12 +16,6 @@
 
     <!-- siimple style -->
     {{ HTML::style('css/site.css') }}
-    
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
   </head>
 
   <body>
@@ -35,7 +29,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand">SIGIMOB</a>
+          <span class="navbar-brand">SIGIMOB</span>
         </div>
         <div class="navbar-collapse collapse">
           <ul class="nav navbar-nav navbar-right">
