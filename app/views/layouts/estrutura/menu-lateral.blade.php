516837628edccfcb68a2a59b82f860529138a9d8
Renan Jhonatha ^-^ <renan@proesc.com>
2015-03-29 20:18:46 -0300

    Retirado user profile

--- app/views/layouts/estrutura/menu-lateral.blade.php
+++ app/views/layouts/estrutura/menu-lateral.blade.php
@@ -1,20 +1,11 @@
 <aside class="main-sidebar">
   <!-- sidebar: style can be found in sidebar.less -->
   <section class="sidebar">
-    <!-- Sidebar user panel -->
-    <div class="user-panel">
-      <div class="pull-left image">
-        <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image" />
-      </div>
-      <div class="pull-left info">
-        <p>Alexander Pierce</p>
-      </div>
-    </div>
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu">
       <li class="header">MENU DE NAVEGAÇÃO</li>
       <li class="active treeview">
-        <a href="#">
+        <a href="{{action('IndexController@index')}}">
           <i class="fa fa-dashboard"></i> <span>Inicio</span>
         </a>
       </li>
@@ -25,7 +16,7 @@
         </a>
       </li>
       <li class="treeview">
-        <a href="#">
+        <a href="{{action('UserController@index')}}">
           <i class="fa fa-users"></i>
           <span>Usuários</span>
         </a>
